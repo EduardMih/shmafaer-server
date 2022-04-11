d2a20ef77384ce6e616cfa9117f0c21becebfa1e
EduardMih <hamza.eduard@yahoo.com>
2022-04-11 14:45:02 +0300

    Initial commit

---
+++ src/main/java/com/licenta/shmafaerserver/ShmafaerServerApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.shmafaerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmafaerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShmafaerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/licenta/shmafaerserver/ShmafaerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.shmafaerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmafaerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
