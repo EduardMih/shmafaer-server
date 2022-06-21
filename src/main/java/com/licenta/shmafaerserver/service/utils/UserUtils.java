547e029899340e06c8c23c2117fe73191b257f88
EduardMih <hamza.eduard@yahoo.com>
2022-06-21 14:04:23 +0300

    check student id by id

--- src/main/java/com/licenta/shmafaerserver/service/utils/UserUtils.java
+++ src/main/java/com/licenta/shmafaerserver/service/utils/UserUtils.java
@@ -7,8 +7,14 @@ import com.licenta.shmafaerserver.exception.CustomExceptions.UserAlreadyExists;
 import com.licenta.shmafaerserver.model.enums.ERole;
 import com.licenta.shmafaerserver.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Component;
 
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Objects;
 
 @Component
@@ -71,11 +77,46 @@ public class UserUtils {
         if(Objects.equals(newUserDTO.getRoleName(), ERole.STUDENT.name()))
         {
 
-            return (newUserDTO.getInstitutionalID() != null) && (newUserDTO.getInstitutionalID().startsWith("310"));
+            return (newUserDTO.getInstitutionalID() != null) && //(newUserDTO.getInstitutionalID().startsWith("310"));
+                    (checkIDJson(newUserDTO.getEmail(), newUserDTO.getInstitutionalID()));
 
         }
 
         return true;
 
+    }
+
+    private boolean checkIDJson(String email, String id)
+    {
+        try
+        {
+            Object obj = new JSONParser().parse(new FileReader("C:\\Users\\hamza\\Students.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray jsonArray = (JSONArray) jsonObject.get("students");
+
+            Iterator itr = jsonArray.iterator();
+            Map<String, String> student;
+
+            while(itr.hasNext())
+            {
+                student = (Map) itr.next();
+
+                if(Objects.equals(student.get("email"), email) && (Objects.equals(student.get("id"), id)))
+                {
+
+                    return true;
+
+                }
+            }
+
+            return false;
+        }
+        catch(Exception e)
+        {
+
+            return false;
+
+        }
     }
 }
